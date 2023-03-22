7f5f7dc0b91b6d487c5e9d6d80a3ab510ff79ebe
Alexey Agafonov <taganroglad1@gmail.com>
2023-03-22 22:42:16 +0300

    43434

--- Exam04/Program.cs
+++ Exam04/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double NumnerA = 12;
+double NumberB = 10;
+Console.WriteLine(NumnerA / NumberB);
